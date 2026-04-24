a2687e306bd2adaf519d974f4b7d8947077fdca6
alexhr30082000-pixel <alexhernandez30082000@gmail.com>
2026-04-23 21:45:39 -0600

    Add files via upload

--- auth.js
+++ auth.js
@@ -5,43 +5,40 @@ import {
   signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.7.0/firebase-auth.js";
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyCAre1FtPQ1mH5sch_l15_QCUrrE6feJTs",
-    authDomain: "ventas-app-pro.firebaseapp.com",
-    projectId: "ventas-app-pro",
-    storageBucket: "ventas-app-pro.firebasestorage.app",
-    messagingSenderId: "142986289176",
-    appId: "1:142986289176:web:4906fdca2901a5b6aae36d"
-  };
+// CONFIG FIREBASE (pon la tuya)
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_DOMINIO",
+  projectId: "TU_ID",
+};
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
 // REGISTRO
-window.registrar = async () => {
+document.getElementById("btnRegister").addEventListener("click", () => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  try {
-    await createUserWithEmailAndPassword(auth, email, password);
-    alert("Usuario registrado correctamente");
-    window.location.href = "index.html";
-  } catch (error) {
-    alert("Error: " + error.message);
-  }
-};
+  createUserWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      document.getElementById("mensaje").textContent = "Usuario creado ✅";
+    })
+    .catch(error => {
+      document.getElementById("mensaje").textContent = error.message;
+    });
+});
 
 // LOGIN
-window.login = async () => {
+document.getElementById("btnLogin").addEventListener("click", () => {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  try {
-    await signInWithEmailAndPassword(auth, email, password);
-    alert("Login exitoso");
-    window.location.href = "ventas.html"; // redirige al CRUD
-  } catch (error) {
-    alert("Error: " + error.message);
-  }
-};
+  signInWithEmailAndPassword(auth, email, password)
+    .then(() => {
+      window.location.href = "agenda.html"; // 🔥 REDIRECCIÓN
+    })
+    .catch(error => {
+      document.getElementById("mensaje").textContent = "Error: " + error.message;
+    });
+});
